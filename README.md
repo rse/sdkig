c3f271ce04dec8c2a7227d9ff3069cbd072d9950
Ralf S. Engelschall <rse@engelschall.com>
2020-03-28 10:20:30 +0100

    initially place under version control

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
-# sdkig
-Stream-Deck Key-Image Generator
+
+SDKIG
+=====
+
+**Stream-Deck Key-Image Generator**
+
+<p/>
+<img src="https://nodei.co/npm/sdkig.png?downloads=true&stars=true" alt=""/>
+
+<p/>
+<img src="https://david-dm.org/rse/sdkig.png" alt=""/>
+
+Abstract
+--------
+
+SDKIG is a small utility for generating key images for Elgato Stream
+Deck devices. The key images have to be of size 288x288 pixels (although
+the device seems to downscale them to 72x72 pixels) and are generated
+by the combination of a FontAwesome icon and a TypoPRO Source Sans Pro
+title. The colors of the background, the icon and the title can be
+choosen through RGB values.
+
+Installation
+------------
+
+```sh
+$ npm install -g sdkig
+```
+
+Usage
+-----
+
+```sh
+$ sdkig \
+    -b bb0000 \
+    -i radiation-alt -I ffffff \
+    -t "Attention"   -T ffc0c0 \
+    -o sample.png
+```
+
+![sample](sample.png)
+
+License
+-------
+
+Copyright &copy; 2020 Dr. Ralf S. Engelschall (http://engelschall.com/)
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
---
+++ sdkig.js
@@ -0,0 +1,216 @@
+#!/usr/bin/env node
+/*!
+**  SDKIG -- Stream-Deck Key-Image Generator
+**  Copyright (c) 2020 Dr. Ralf S. Engelschall <rse@engelschall.com>
+**
+**  Permission is hereby granted, free of charge, to any person obtaining
+**  a copy of this software and associated documentation files (the
+**  "Software"), to deal in the Software without restriction, including
+**  without limitation the rights to use, copy, modify, merge, publish,
+**  distribute, sublicense, and/or sell copies of the Software, and to
+**  permit persons to whom the Software is furnished to do so, subject to
+**  the following conditions:
+**
+**  The above copyright notice and this permission notice shall be included
+**  in all copies or substantial portions of the Software.
+**
+**  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+**  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+**  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+**  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+**  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+**  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+**  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+/*  own package information  */
+const my          = require("./package.json")
+
+/*  internal requirements  */
+const fs          = require("fs")
+
+/*  external requirements  */
+const yargs       = require("yargs")
+const chalk       = require("chalk")
+const tmp         = require("tmp")
+const Prince      = require("prince")
+const execa       = require("execa")
+
+;(async () => {
+    /*  command-line option parsing  */
+    const argv = yargs()
+        /* eslint indent: off */
+        .parserConfiguration({
+            "duplicate-arguments-array": false,
+            "set-placeholder-key":       true,
+            "flatten-duplicate-arrays":  true,
+            "camel-case-expansion":      true,
+            "strip-aliased":             false,
+            "dot-notation":              false,
+            "halt-at-non-option":        true
+        })
+        .usage("Usage: sdkig " +
+            "[-h|--help] " +
+            "[-V|--version] " +
+            "[-b|--background-color <rgb-color>] " +
+            "[-i|--icon-name <name>] " +
+            "[-I|--icon-color <rgb-color>] " +
+            "[-t|--title-text <text>] " +
+            "[-T|--title-color <rgb-color>] " +
+            "[-o|--output-file <png-file>]"
+        )
+        .option("h", {
+            describe: "show program help information",
+            alias:    "help", type: "boolean", default: false
+        })
+        .option("V", {
+            describe: "show program version information",
+            alias:    "version", type: "boolean", default: false
+        })
+        .option("b", {
+            describe: "background color",
+            alias:    "background-color", type: "string", nargs: 1, default: "#000000"
+        })
+        .option("i", {
+            describe: "icon name",
+            alias:    "icon-name", type: "string", nargs: 1, default: ""
+        })
+        .option("I", {
+            describe: "icon color",
+            alias:    "icon-color", type: "string", nargs: 1, default: "#ffffff"
+        })
+        .option("t", {
+            describe: "title text",
+            alias:    "title-text", type: "string", nargs: 1, default: ""
+        })
+        .option("T", {
+            describe: "title color",
+            alias:    "title-color", type: "string", nargs: 1, default: "#e0e0e0"
+        })
+        .option("o", {
+            describe: "output file",
+            alias:    "output-file", type: "string", nargs: 1, default: ""
+        })
+        .version(false)
+        .strict(true)
+        .showHelpOnFail(true)
+        .demand(0)
+        .parse(process.argv.slice(2))
+
+    /*  short-circuit processing of "-V" (version) command-line option  */
+    if (argv.version) {
+        process.stderr.write(`${my.name} ${my.version} <${my.homepage}>\n`)
+        process.stderr.write(`${my.description}\n`)
+        process.stderr.write(`Copyright (c) 2020 ${my.author.name} <${my.author.url}>\n`)
+        process.stderr.write(`Licensed under ${my.license} <http://spdx.org/licenses/${my.license}.html>\n`)
+        process.exit(0)
+    }
+
+    /*  sanity check usage  */
+    if (argv.icon === "")
+        throw new Error("icon has to be defined")
+    if (argv.text === "")
+        throw new Error("text has to be defined")
+    if (argv.output === "")
+        throw new Error("output has to be defined")
+
+    /*  generate HTML  */
+    let htmlFile = tmp.fileSync()
+    let html =
+        `<!DOCTYPE html>
+        <html>
+            <head>
+                <title></title>
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <style type="text/css">
+                    @import "${process.cwd()}/node_modules/@fortawesome/fontawesome-free/css/all.min.css";
+                    @import "${process.cwd()}/node_modules/typopro-web/web/TypoPRO-SourceSansPro/TypoPRO-SourceSansPro.css";
+                    @page {
+                        size: 288px 288px;
+                        margin: 0;
+                        padding: 0;
+                    }
+                    html, body {
+                        margin: 0;
+                        padding: 0;
+                        width: 288px;
+                        height: 288px;
+                        background-color: #000000;
+                        position: relative;
+                    }
+                    .canvas {
+                        position: absolute;
+                        left: 0;
+                        top: 0;
+                        border-radius: 40px;
+                        width: 100%;
+                        height: 100%;
+                        background-color: #${argv.backgroundColor};
+                    }
+                    .icon {
+                        position: absolute;
+                        left: 0;
+                        top: 15px;
+                        width: 100%;
+                        height: 100%;
+                        text-align: center;
+                        font-size: 220px;
+                        color: #${argv.iconColor};
+                    }
+                    .text {
+                        position: absolute;
+                        left: 0;
+                        bottom: 0px;
+                        width: 100%;
+                        text-align: center;
+                        font-family: "TypoPRO Source Sans Pro";
+                        font-size: 50px;
+                        color: #${argv.titleColor};
+                    }
+                </style>
+            </head>
+            <body>
+                <div class="canvas">
+                    <i class="icon fa fa-${argv.iconName}"></i>
+                    <div class="text">${argv.titleText}</div>
+                </div>
+            </body>
+        </html>`
+    await fs.promises.writeFile(htmlFile.name, html, { encoding: "utf8" })
+
+    /*  render HTML into PDF  */
+    let pdfFile = tmp.fileSync()
+    await Prince()
+        .inputs(htmlFile.name)
+        .output(pdfFile.name)
+        .execute()
+        .catch((err) => { throw err })
+
+    /*  render PDF into PNG  */
+    let pngFile = tmp.fileSync()
+    await execa("pdftocairo", [
+        "-png",
+        "-singlefile",
+        "-scale-to", "288",
+        pdfFile.name,
+        pngFile.name
+    ])
+
+    /*  copy PNG to output file
+        (notice: pdftocairo(1) always appends a nasty ".png" extension)  */
+    await fs.promises.copyFile(`${pngFile.name}.png`, argv.outputFile)
+
+    /*  cleanup temporary files  */
+    htmlFile.removeCallback()
+    pdfFile.removeCallback()
+    pngFile.removeCallback()
+
+    /*  terminate gracefully  */
+    process.exit(0)
+})().catch((err) => {
+    /*  fatal error  */
+    process.stderr.write(`sdkig: ${chalk.red("ERROR:")} ${err.stack}\n`)
+    process.exit(1)
+})
+
