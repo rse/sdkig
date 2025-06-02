720516da3a7339ba168c5e61db5a4c29908ea5ae
Ralf S. Engelschall <rse@engelschall.com>
2025-06-02 08:51:25 +0200

    update year in all copyright messages

--- sdkig.js
+++ sdkig.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 /*!
 **  SDKIG -- Stream-Deck Key-Image Generator
-**  Copyright (c) 2020-2024 Dr. Ralf S. Engelschall <rse@engelschall.com>
+**  Copyright (c) 2020-2025 Dr. Ralf S. Engelschall <rse@engelschall.com>
 **
 **  Permission is hereby granted, free of charge, to any person obtaining
 **  a copy of this software and associated documentation files (the
@@ -111,7 +111,7 @@ const execa       = require("execa")
     if (argv.version) {
         process.stderr.write(`${my.name} ${my.version} <${my.homepage}>\n`)
         process.stderr.write(`${my.description}\n`)
-        process.stderr.write(`Copyright (c) 2020-2024 ${my.author.name} <${my.author.url}>\n`)
+        process.stderr.write(`Copyright (c) 2020-2025 ${my.author.name} <${my.author.url}>\n`)
         process.stderr.write(`Licensed under ${my.license} <http://spdx.org/licenses/${my.license}.html>\n`)
         process.exit(0)
     }
